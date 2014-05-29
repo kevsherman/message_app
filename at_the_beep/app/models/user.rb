6703684b25a1638de3280925edabfaaaac2bc8dd
Kevin Sherman <kevsherman@gmail.com>
2014-05-28 20:44:10 -0700

    improved user validations, improved site navigation

--- at_the_beep/app/models/user.rb
+++ at_the_beep/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :messages, through: :events
 
   validates :email, uniqueness: true
+  validates :firstname, length:{minimum: 2}
+  validates :lastname, length:{minimum: 2}
+  # validates :password, length:{minimum: 6}
+
 end
