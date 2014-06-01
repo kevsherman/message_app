6ddbe638ee6808364adf9e1da4896a3cdd5c635b
Kevin Sherman <kevsherman@gmail.com>
2014-05-31 18:02:21 -0700

    updated views for login and signup

--- at_the_beep/app/models/event.rb
+++ at_the_beep/app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ActiveRecord::Base
   has_many :messages
   belongs_to :user   # make sure this is also specified in the migration that created the tableso the reference field and indexes are created
   validates :url, presence: true
+  validates :name, presence: true, length: {minimum: 4}
 
 before_validation :generate_url, on: :create 
 
--- at_the_beep/public/javascript/application.js
+++ at_the_beep/public/javascript/application.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  function HeaderController($scope, $location) 
+{ 
+    $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+}
 });
+
