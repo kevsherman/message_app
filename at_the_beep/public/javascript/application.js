74f0ffa39b39f17871bdb26d79c269e9f525aae6
Kevin Sherman <kevsherman@gmail.com>
2014-06-01 16:31:59 -0700

    added more styling to homepage

--- at_the_beep/public/javascript/application.js
+++ at_the_beep/public/javascript/application.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-  function HeaderController($scope, $location) 
-{ 
-    $scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
-    };
-}
+  
 });
 
